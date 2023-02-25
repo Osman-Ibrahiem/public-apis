da21e884a4d9a158de5e65ad1d61aabae5c7ad29
Osman Ibrahiem <osman.ibrahiem187@gmail.com>
2023-02-25 21:28:04 +0200

    Resolve dagger-hilt cache module dependencies

--- app/src/main/java/com/fawry/publicapis/di/cache/DataBaseModule.kt
+++ app/src/main/java/com/fawry/publicapis/di/cache/DataBaseModule.kt
@@ -2,6 +2,7 @@ package com.fawry.publicapis.di.cache
 
 import android.content.Context
 import com.fawry.cache.database.EntriesDataBase
+import com.fawry.cache.utils.PreferencesHelper
 import com.fawry.publicapis.di.annotations.qualifiers.AppContext
 import dagger.Module
 import dagger.Provides
@@ -19,5 +20,13 @@ object DataBaseModule {
         @AppContext context: Context,
     ): EntriesDataBase {
         return EntriesDataBase.getInstance(context)
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferenceHelper(
+        @AppContext context: Context,
+    ): PreferencesHelper {
+        return PreferencesHelper(context)
     }
 }
