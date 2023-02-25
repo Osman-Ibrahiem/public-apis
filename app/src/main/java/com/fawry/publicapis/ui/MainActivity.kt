8015b21b633a5a98313a07afc069bc52c8eaa92f
Osman Ibrahiem <osman.ibrahiem187@gmail.com>
2023-02-25 21:20:27 +0200

    Added navigation graph and empty fragments

--- app/src/main/java/com/fawry/publicapis/ui/MainActivity.kt
+++ app/src/main/java/com/fawry/publicapis/ui/MainActivity.kt
@@ -1,54 +1,19 @@
 package com.fawry.publicapis.ui
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import com.fawry.presentation.viewmodel.CategoriesState
-import com.fawry.presentation.viewmodel.CategoriesViewModel
 import com.fawry.publicapis.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: CategoriesViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setObservers()
-    }
-
-    private fun setObservers() {
-        viewModel.stateObservable.observe(this) { categoriesState ->
-            updateView(categoriesState)
-        }
-    }
-
-    private fun updateView(categoriesState: CategoriesState) {
-        when (categoriesState) {
-            is CategoriesState.Success -> {
-                binding.progressBar.isVisible = false
-                binding.textView.isVisible = true
-                binding.textView.text = categoriesState.categories.joinToString { it.category }
-            }
-            is CategoriesState.Error -> {
-                binding.progressBar.isVisible = false
-                binding.textView.isVisible = true
-                binding.textView.text = getString(categoriesState.message)
-            }
-            CategoriesState.Init -> {
-                binding.textView.isVisible = false
-                binding.progressBar.isVisible = false
-            }
-            CategoriesState.Loading -> {
-                binding.progressBar.isVisible = true
-                binding.textView.isVisible = false
-            }
-        }
     }
 
 }
