8f1618e84727aced5bb3cc8744db64c61968394e
Osman Ibrahiem <osman.ibrahiem187@gmail.com>
2023-02-25 21:25:00 +0200

    Added navigation setup

--- app/src/main/java/com/fawry/publicapis/ui/MainActivity.kt
+++ app/src/main/java/com/fawry/publicapis/ui/MainActivity.kt
@@ -2,18 +2,34 @@ package com.fawry.publicapis.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.fawry.publicapis.R
 import com.fawry.publicapis.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val navController: NavController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navigationHostFragment) as NavHostFragment
+        navHostFragment.navController
+    }
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupActionBarWithNavController(navController)
     }
 
 }
